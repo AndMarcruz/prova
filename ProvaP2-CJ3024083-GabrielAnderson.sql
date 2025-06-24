862f618901083cd2ed098e08f951ef192d624772
AndMarcruz <140858818+AndMarcruz@users.noreply.github.com>
2025-06-23 21:44:16 -0300

    Update and rename ProvaP2-CJ3024083.sql to ProvaP2-CJ3024083-GabrielAnderson.sql

--- ProvaP2-CJ3024083-GabrielAnderson.sql
+++ ProvaP2-CJ3024083-GabrielAnderson.sql
@@ -1,6 +1,6 @@
 -- Prova P2 - 2024
 -- Prova P2 - ALUNOS.sql
--- Solução da Prova P2
+-- SoluÃ§Ã£o da Prova P2
 -----------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------
@@ -11,11 +11,11 @@
 
 
 -----------------------------------------------------------------------------------
--- PARTE A: CRIAÇÃO DO BANCO DE DADOS E DAS TABELAS QUE SERÃO UTILIZADAS
+-- PARTE A: CRIAÃ‡ÃƒO DO BANCO DE DADOS E DAS TABELAS QUE SERÃƒO UTILIZADAS
 -----------------------------------------------------------------------------------
 
 
--- * Instruções: Execute os comandos abaixo para criar o banco de dados.
+-- * InstruÃ§Ãµes: Execute os comandos abaixo para criar o banco de dados.
 
 
 USE master;
@@ -32,7 +32,7 @@ USE LOJAMARIA3024083;
 GO
 
 
--- 2. Instruções: Execute os comandos abaixo para criar as tabelas
+-- 2. InstruÃ§Ãµes: Execute os comandos abaixo para criar as tabelas
 
 
 -- Cria a tabela CLIENTES
@@ -94,29 +94,29 @@ GO
 
 
 -----------------------------------------------------------------------------------
--- PARTE B: INSERÇÃO DOS DADOS NAS TABELAS
+-- PARTE B: INSERÃ‡ÃƒO DOS DADOS NAS TABELAS
 -----------------------------------------------------------------------------------
 
 
--- * Instruções: Execute os comandos abaixo para inserir os dados nas tabelas.
+-- * InstruÃ§Ãµes: Execute os comandos abaixo para inserir os dados nas tabelas.
 
 
 --  Insere os clientes 
 INSERT INTO CLIENTES VALUES 
-    (720, 'Ana', 'Rua 17, n. 19', 'Niterói', '24358310', 'RJ', '12113231/0001-34', '2134'),
-    (870, 'Flávio', 'Av. Pres. Vargas, n. 10', 'São Paulo', '22763931', 'SP', '22534126/9387-9', '4631'), 
+    (720, 'Ana', 'Rua 17, n. 19', 'NiterÃ³i', '24358310', 'RJ', '12113231/0001-34', '2134'),
+    (870, 'FlÃ¡vio', 'Av. Pres. Vargas, n. 10', 'SÃ£o Paulo', '22763931', 'SP', '22534126/9387-9', '4631'), 
     (110, 'Jorge', 'Rua Caiapo, n. 13', 'Curitiba', '30078500', 'PR', '14512764/9834-9', null), 
-    (222, 'Lúcia', 'Rua Itabira 123, Loja 9', 'Belo Horizonte', '22124391', 'MG', '28315213/9348-8', '2985'),
-    (830, 'Maurício', 'Av. Paulista 1236, sl/2345', 'São Paulo', '3012683', 'SP', '32816985/7465-6', '9343'), 
+    (222, 'LÃºcia', 'Rua Itabira 123, Loja 9', 'Belo Horizonte', '22124391', 'MG', '28315213/9348-8', '2985'),
+    (830, 'MaurÃ­cio', 'Av. Paulista 1236, sl/2345', 'SÃ£o Paulo', '3012683', 'SP', '32816985/7465-6', '9343'), 
     (130, 'Edmar', 'Rua da Praia s/n', 'Salvador', '30079300', 'BA', '23463284/234-9', '7121'), 
     (410, 'Rodolfo', 'Largo da Lapa 27, sobrado', 'Rio de Janeiro', '30078900', 'RJ', '12835128/2346-9', '7431'), 
-    (20, 'Beth', 'Av. Climério, n. 45', 'São Paulo', '25679300', 'SP', '32485126/7326-8', '9280'), 
+    (20, 'Beth', 'Av. ClimÃ©rio, n. 45', 'SÃ£o Paulo', '25679300', 'SP', '32485126/7326-8', '9280'), 
     (157, 'Paulo', 'Tv. Moraes c/3', 'Londrina', null, 'PR', '32848223/324-2', '1923'), 
-    (180, 'Lívio', 'Av. Beira Mar, n. 1256', 'Florianópolis', '30077500', 'SC', '12736571/2347-4', null), 
-    (260, 'Susana', 'Rua Lopes Mendes, n. 12', 'Niterói', '30046500', 'RJ', '21763571/232-9', '2530'),
-    (290, 'Renato', 'Rua Meireles, n. 123, bl. 2 sl. 345', 'São Paulo', '30225900', 'SP', '13276571/1231-4', '1820'), 
-    (390, 'Sebastião', 'Rua da Igreja, n. 10', 'Uberaba', '30438700', 'MG', '32176547/213-3', '9071'), 
-    (234, 'José', 'Quadra 3 bl. 3 sl. 1003', 'Brasília', '22841650', 'DF', '21763576/1232-3', '2931');
+    (180, 'LÃ­vio', 'Av. Beira Mar, n. 1256', 'FlorianÃ³polis', '30077500', 'SC', '12736571/2347-4', null), 
+    (260, 'Susana', 'Rua Lopes Mendes, n. 12', 'NiterÃ³i', '30046500', 'RJ', '21763571/232-9', '2530'),
+    (290, 'Renato', 'Rua Meireles, n. 123, bl. 2 sl. 345', 'SÃ£o Paulo', '30225900', 'SP', '13276571/1231-4', '1820'), 
+    (390, 'SebastiÃ£o', 'Rua da Igreja, n. 10', 'Uberaba', '30438700', 'MG', '32176547/213-3', '9071'), 
+    (234, 'JosÃ©', 'Quadra 3 bl. 3 sl. 1003', 'BrasÃ­lia', '22841650', 'DF', '21763576/1232-3', '2931');
 GO
 
 
@@ -126,15 +126,15 @@ GO
 
 --  Insere os vendedores
 INSERT INTO VENDEDORES VALUES 
-    (209, 'José', 1800.00,'C'),
+    (209, 'JosÃ©', 1800.00,'C'),
     (111, 'Carlos', 2490.00, 'A'),
-    (11, 'João', 2780.00, 'C'),
-    (240, 'Antônio', 9500.00, 'C'),
+    (11, 'JoÃ£o', 2780.00, 'C'),
+    (240, 'AntÃ´nio', 9500.00, 'C'),
     (720, 'Felipe', 4600.00, 'A'),
     (213, 'Jonas', 2300.00, 'A'),
-    (101, 'João', 2650.00, 'C'),
+    (101, 'JoÃ£o', 2650.00, 'C'),
     (310, 'Josias', 870, 'B'),
-    (250, 'Maurício', 2930.00, 'B');
+    (250, 'MaurÃ­cio', 2930.00, 'B');
 GO
 
 
@@ -148,7 +148,7 @@ INSERT INTO PRODUTOS VALUES
     (31, 'Chocolate','BAR', 0.87),
     (78, 'Vinho','L', 2.00),
     (22, 'Linho','M', 0.11),
-    (30, 'Açúcar','SAC', 0.30),
+    (30, 'AÃ§Ãºcar','SAC', 0.30),
     (53, 'Linha','M', 1.80),
     (13, 'Ouro','G', 6.18),
     (45, 'Madeira','M', 0.25),
@@ -233,23 +233,23 @@ GO
 -----------------------------------------------------------------------------------
 
 
--- * Instruções: Escreva os códigos SQL para realizar as consultas abaixo.
+-- * InstruÃ§Ãµes: Escreva os cÃ³digos SQL para realizar as consultas abaixo.
 
 
--- 1. Liste o nome e a data da criação das tabelas que foram criadas. 
+-- 1. Liste o nome e a data da criaÃ§Ã£o das tabelas que foram criadas. 
 -- Utilize aliases para exibir o nome das colunas.(0.5 ponto)
 Select name as 'Nome',
-	create_date as 'Data de criação'
+	create_date as 'Data de criaÃ§Ã£o'
 from  sys.tables;
 go
 
 
 
--- 2. Liste o código, nome, endereço, cidade e estado dos clientes. Exiba as 
--- informações somente dos clientes que forem do estado de São Paulo ou de Santa 
+-- 2. Liste o cÃ³digo, nome, endereÃ§o, cidade e estado dos clientes. Exiba as 
+-- informaÃ§Ãµes somente dos clientes que forem do estado de SÃ£o Paulo ou de Santa 
 -- Catarina. Utilize aliases para exibir o nome das colunas. (1.0 ponto)
 
-select CodigoCliente as 'Código do cliente',
+select CodigoCliente as 'CÃ³digo do cliente',
     NomeCliente   as 'Nome',
     Endereco,
     Cidade,
@@ -260,17 +260,17 @@ go
 
  
 
--- 3. Liste o valor do maior e do menor preço para dos produtos armazenados no 
+-- 3. Liste o valor do maior e do menor preÃ§o para dos produtos armazenados no 
 -- banco de dados. Utilize aliases para exibir o nome das colunas. (1.0 ponto)
 select DescricaoProduto as 'Nome do Produto',
-	ValorUnitario as 'Preço'
+	ValorUnitario as 'PreÃ§o'
 
 from PRODUTOS
 Order by ValorUnitario Desc;
 go
 
 select DescricaoProduto as 'Nome do Produto',
-	ValorUnitario as 'Preço'
+	ValorUnitario as 'PreÃ§o'
 
 from PRODUTOS
 Order by ValorUnitario Asc;
@@ -279,20 +279,20 @@ go
 
 
 
--- 4. Listar o número do pedido, o código do produto e a quantidade dos itens do 
+-- 4. Listar o nÃºmero do pedido, o cÃ³digo do produto e a quantidade dos itens do 
 -- pedido com a quantidade igual a 35 da tabela ITEM_DE_PEDIDO. Utilize aliases 
 -- para exibir o nome das colunas. (1.0 ponto)
-select NumeroPedido as 'Número do pedido',
-	CodigoProduto as 'Código do produto',
+select NumeroPedido as 'NÃºmero do pedido',
+	CodigoProduto as 'CÃ³digo do produto',
 	Quantidade
 where Quantidade = (35)
 from item_de_pedido;
 go
 
--- 5. Listar o código, nome e salário dos vendedores cujo nome comece com A, C, 
+-- 5. Listar o cÃ³digo, nome e salÃ¡rio dos vendedores cujo nome comece com A, C, 
 -- F ou M. Ordene o resultado pelo nome do vendedor. Utilize aliases para exibir 
 -- o nome das colunas. (1.0 ponto)
-select CodigoVendedor as 'Código do vendedor',
+select CodigoVendedor as 'CÃ³digo do vendedor',
 	NomeVendedor as 'Nome do Vendedor',
 	Salario
 where '%' like [A,C,F,M]
@@ -302,7 +302,7 @@ go
 
 
 
--- 6. Listar todos os dados dos clientes que não tenham número de inscrição 
+-- 6. Listar todos os dados dos clientes que nÃ£o tenham nÃºmero de inscriÃ§Ã£o 
 -- estadual (IE). (1.0 ponto)
 Select * 
 where IE<> null
@@ -311,10 +311,10 @@ go
 
 
 
--- 7. Exibir o número do pedido, a quantidade total de produtos diferentes que 
+-- 7. Exibir o nÃºmero do pedido, a quantidade total de produtos diferentes que 
 -- existem em cada pedido e o total geral de itens que existem em cada pedido.
 -- Utilize aliases para exibir o nome das colunas. (1.0 ponto)
-select NumeroPedido as 'Número do Pedido',
+select NumeroPedido as 'NÃºmero do Pedido',
 	count [CodigoProduto] as 'Quantidade de itens diferentes',
 	Quantidade
 from itens_de_pedidos;
@@ -322,10 +322,10 @@ go
 
 
 
--- 8. Exibir o código, nome, salário e média salarial dos vendedores. Exibir
--- somente as informações dos vendedores que ganham acima da média salarial.
+-- 8. Exibir o cÃ³digo, nome, salÃ¡rio e mÃ©dia salarial dos vendedores. Exibir
+-- somente as informaÃ§Ãµes dos vendedores que ganham acima da mÃ©dia salarial.
 -- Utilize aliases para exibir o nome das colunas. (1.5 pontos)
-select CodigoVendedor as 'Código do vendedor',
+select CodigoVendedor as 'CÃ³digo do vendedor',
 	NomeVendedor as 'Nome do vendedor',
 	salarios,
 	AVG salarios
@@ -334,16 +334,16 @@ where salarios > avg
  go
 
 
--- 9. Exibir o código do cliente, seu nome, o produto que ele comprou, o valor
--- unitário desse produto, a quantidade que foi comprada, o valor total a ser 
+-- 9. Exibir o cÃ³digo do cliente, seu nome, o produto que ele comprou, o valor
+-- unitÃ¡rio desse produto, a quantidade que foi comprada, o valor total a ser 
 -- pago, o prazo de entrega do pedido e o nome do vendedor que efetuou a venda. 
 -- Ordene o resultado pelo nome do cliente, nome do produto, quantidade de itens 
 -- que foram comprados, o prazo de entrega do pedido e o nome do vendedor.
 -- Utilize aliases para exibir o nome das colunas. (2.0 pontos)
-select CodigoCliente as 'Código do cliente',
+select CodigoCliente as 'CÃ³digo do cliente',
 	NomeCliente as 'Nome do cliente',
 	DescricaoProduto as 'Nome do produto',
-	ValorUnitario as 'Preço Unitário',
+	ValorUnitario as 'PreÃ§o UnitÃ¡rio',
 	Quantidade,
 	PrazoEntrega as 'Prazo de entrega',
 	NomeVendedor as 'Nome do vendedor'
@@ -359,7 +359,7 @@ go
 -----------------------------------------------------------------------------------
 
 
--- * Instruções: Execute os comandos abaixo para remover o banco de dados.
+-- * InstruÃ§Ãµes: Execute os comandos abaixo para remover o banco de dados.
 
 
 USE master;
